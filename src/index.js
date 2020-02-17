f09796c7f055464f692d51f12c2b4efac4d8b731
Nada Nisrina Septiana <nisrinan10@gmail.com>
2020-02-17 15:25:42 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () =>{
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Nada
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 07:00 PM</span>
+                    </div>
+                    <div className="text">
+                        Nice blog post !
+                    </div>
+                </div>
+            </div>
+           
+        </div>
+        );
+    };
+    
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+    );
